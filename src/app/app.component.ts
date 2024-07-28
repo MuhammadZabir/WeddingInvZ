890d4c480459431f2eb842e39cdb106996684f65
Muhammad Zabir <tsonouichi@gmail.com>
2024-07-28 18:19:26 +0800

    Add ng-bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, NgbModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.less'
 })
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,16 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { provideAnimations } from '@angular/platform-browser/animations';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration()]
+  providers: [
+    provideAnimations(),
+    importProvidersFrom(NgbModule),
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideClientHydration()
+  ]
 };
