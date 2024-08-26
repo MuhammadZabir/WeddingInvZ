71f491f3c4794a20d0bb8f5c0ed268adf16e05f1
Muhammad Zabir <tsonouichi@gmail.com>
2024-08-26 21:43:25 +0800

    trial

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,12 +5,16 @@ import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { CommonModule } from '@angular/common';
-import { HttpClientModule, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { FirstPageComponent } from './first-page/first-page.component';
+import { FourthPageComponent } from './fourth-page/fourth-page.component';
+import { SecondPageComponent } from './second-page/second-page.component';
+import { ThirdPageComponent } from './third-page/third-page.component';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideAnimations(),
-    importProvidersFrom(NgbModule, BrowserAnimationsModule, CommonModule),
+    importProvidersFrom(NgbModule, BrowserAnimationsModule, CommonModule, FirstPageComponent, SecondPageComponent, ThirdPageComponent, FourthPageComponent),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideClientHydration(),
